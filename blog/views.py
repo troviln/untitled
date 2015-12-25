6f0c4b388362d045a33c55d043e9bd3f92383a93
troviln <xomenko2@gmail.com>
2015-12-25 16:29:51 +0200

    Test5

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+
 
 
 def post_list(request):
@@ -11,3 +13,46 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+     if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+     else:
+        form = PostForm()
+     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+
+def post_change(request, pk):
+    before_edit = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=before_edit)
+        if form.is_valid():
+             post = form.save(commit=False)
+             post.save()
+             return redirect('post_detail', pk=pk)
+    if pk:
+        form = PostForm(instance=before_edit)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_change.html', {'form': form})
+
